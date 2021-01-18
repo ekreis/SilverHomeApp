9d7d0a00425785e588ad98c9fe4f5d5bd457e8ca
Erin Kreis <erinkreis2@gmail.com>
2021-01-18 16:29:13 -0500

    Created config folder with default colors and styles

---
+++ components/config/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#476871",
+  secondary: "#d8dfe3",
+  medium: "#b5c3c6",
+  accent: "#efbeab",
+  background: "#e5e5e5",
+  dark: "#3d3d3d",
+  black: "#000",
+  white: "#fff",
+};
