c831f949c1fdd74f3846e9d5edf5f7d542d7c797
Erin Kreis <erinkreis2@gmail.com>
2021-01-21 18:12:24 -0500

    Created default Home Screen

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import colors from "../config/colors";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import Header from "../Header";
 import Screen from "../Screen";
+import ServiceButton from "../ServiceButton";
 
 function HomeScreen(props) {
   return (
     <View style={styles.container}>
       <Header title="Home" />
+      <View style={styles.buttonContainer}>
+        <ServiceButton title="Overview" icon="book" />
+        <ServiceButton title="User Info" icon='user'/>
+        <ServiceButton title="Add Services" icon='plus'/>
+        {/* <ServiceButton />
+        <ServiceButton />
+        <ServiceButton /> */}
+      </View>
     </View>
   );
 }
@@ -16,6 +26,15 @@ function HomeScreen(props) {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+    alignContent: "flex-start",
+    paddingTop: 10,
+    paddingBottom: 10,
+    overflow: "hidden",
+  },
   container: {
     backgroundColor: colors.background,
     flex: 1,
