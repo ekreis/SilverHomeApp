796a51fa26d4ce27d00ba25389db0dfc1bf1ffdd
Byron Panrudkevich <bpanrudkevich@gmail.com>
2021-01-22 01:26:25 -0500

    Updated LoginScreen screen

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import HomeScreen from './components/screens/HomeScreen';
+import LoginScreen from './components/screens/LoginScreen';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <LoginScreen />
     </View>
   );
 }
